8d423331df35ab010d80e3faf77a73e16cc096be
Eric Desseaux <eric.desseaux@orange.fr>
2023-02-03 12:44:40 +0100

    Adding comments in root index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/* Root file 
+  Contains the router logic. (react-router-dom version 6 hence why Routes component is used instead of Switch)
+  A "universal" route captures everything that is not setup prior and shoots the 404 error page.
+  This includes the error redirect set in the Housing page. As the error route is not specifically parameter, the
+  universal catches it up.
+*/
 import "./utils/styles/globalStyle.css"
 import React from "react"
 import ReactDOM from "react-dom/client"
